ed73db0b405a37e810e48f30b81c539d963e88c3
Shardul <89443909+ShardulMorecode@users.noreply.github.com>
2025-08-10 16:44:49 +0530

    Add next.config.ts to app folder for package recognition

--- whatsapp-clone-frontend/next.config.ts
+++ whatsapp-clone-frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Generate static files for deployment on Render
 };
 
 export default nextConfig;
